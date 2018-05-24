3816faeb784c4cc90494236b7e5e2e10e058c88d
rayane yagoubi <ryagoubi01@gmail.com>
2018-05-24 16:16:03 +0200

    board.vue

--- src/components/board.vue
+++ src/components/board.vue
@@ -1,10 +1,18 @@
 <<template>
   
+    
 </template>
 <<script>
 import Board from "@/utils/board"
 export default {
-  
+  name: 'board',
+  components: {},
+  computed: {},
+  methods: {}
+  },
+  data (){}
+  },
+  created () {}
 }
 </script>
 <<style>
